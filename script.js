30c92e51b4d282e8d22d8fa7dc7bc01fdd419e68
jishnu <jishnupunathil000@gmail.com>
2022-04-22 19:24:02 +0530

    login script uploaded

---
+++ script.js
@@ -0,0 +1,24 @@
+let login=document.getElementById("login")
+let email=document.getElementById("typeEmailX-2")
+let pwd=document.getElementById("typePasswordX-2")
+let phint=document.getElementById("passhint")
+
+
+
+
+var check = (callback)=>{
+    if(email.value == "admin",
+    pwd.value == 12345){
+        callback();
+        phint.style.color = "green";
+        phint.innerHTML = "Done!!";
+        return true;
+    }else{
+        phint.style.color = "red";
+        phint.innerHTML = "Invalid Username or Password";
+        return false;
+    }
+
+function redirect(){
+    login.action="content.html";
+}
